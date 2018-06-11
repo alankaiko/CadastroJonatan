a9532e1c1e7c536837c03dbf7f1fa7137e62c367
alankaiko <alamdfs@hotmail.com>
2018-06-11 10:26:13 -0300

    criando paginas

--- src/main/java/br/com/jonatan/controller/ClienteController.java
+++ src/main/java/br/com/jonatan/controller/ClienteController.java
@@ -1,8 +1,13 @@
 package br.com.jonatan.controller;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
 import br.com.jonatan.domain.Cliente;
 import br.com.jonatan.service.ClienteService;
 
+@ManagedBean
+@ViewScoped
 public class ClienteController {
 	private Cliente cliente;
 	private ClienteService service;
@@ -20,6 +25,26 @@ public class ClienteController {
 	public void Excluindo(Cliente cliente){
 		this.service.Excluir(this.cliente);
 	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	
 	
 	
 }
