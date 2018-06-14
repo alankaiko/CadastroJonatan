9c68ec336be5f6452fdc7927876bb6880ba92bb2
alankaiko <alamdfs@hotmail.com>
2018-06-14 11:16:01 -0300

    tela cadastro finali

--- src/main/java/br/com/jonatan/controller/ClienteController.java
+++ src/main/java/br/com/jonatan/controller/ClienteController.java
@@ -1,16 +1,22 @@
 package br.com.jonatan.controller;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.jonatan.domain.Cliente;
+import br.com.jonatan.domain.TipoPessoa;
 import br.com.jonatan.service.ClienteService;
+import br.com.jonatan.util.ListasUtil;
 
 @ManagedBean
 @ViewScoped
 public class ClienteController {
 	private Cliente cliente;
 	private ClienteService service;
+	private List<String> listaEstados;
+	private TipoPessoa tipoPessoa;
 	
 	public ClienteController() {
 		this.cliente = new Cliente();
@@ -27,7 +33,9 @@ public class ClienteController {
 	}
 
 
-	
+	public void PuxarListaEstado(){
+		listaEstados = ListasUtil.Estados();
+	}
 	
 	
 	
@@ -41,6 +49,21 @@ public class ClienteController {
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
+	}
+
+
+	public List<String> getListaEstados() {
+		return listaEstados;
+	}
+
+
+	public TipoPessoa getTipoPessoa() {
+		return tipoPessoa;
+	}
+
+
+	public void setTipoPessoa(TipoPessoa tipoPessoa) {
+		this.tipoPessoa = tipoPessoa;
 	}
 	
 	
